cfe0b50492730408afa1225d4026575078a49368
Yuri Tagomori <goncalvessyuri@gmail.com>
2023-05-24 17:09:50 -0300

    continue later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Movie.quote
+  )
+  article.save!
+end
